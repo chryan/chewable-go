7fa3d1caa720b85c12db642a38cc72549d77aee9
Ryan Chew <ryanchew@gmail.com>
2013-10-11 01:02:31 -0700

    Add missing type factory files.

---
+++ typefactory.go
@@ -0,0 +1,50 @@
+// Copyright (C) 2013 Ryan Chew. All rights reserved.
+// Use of this source code is governed by a Apache 2.0
+// license that can be found in the LICENSE file.
+
+package cbl
+
+import (
+	"reflect"
+)
+
+type typeFactory struct {
+	typeMap map[string]reflect.Type
+}
+
+var TypeFactory *typeFactory = &typeFactory{typeMap: make(map[string]reflect.Type)}
+
+func (t *typeFactory) Add(addtype reflect.Type) *typeFactory {
+	if kind := addtype.Kind(); kind == reflect.Ptr || kind == reflect.Interface {
+		addtype = addtype.Elem()
+	}
+
+	name := addtype.String()
+	if _, ok := t.typeMap[name]; !ok {
+		t.typeMap[name] = addtype
+	}
+
+	return t
+}
+
+func (t *typeFactory) New(typename, pkgname string) interface{} {
+	if typ, ok := t.typeMap[pkgname + "." + typename]; ok {
+		return reflect.New(typ).Elem().Interface()
+	}
+	return nil
+}
+
+func (t *typeFactory) Get(typename, pkgname string) reflect.Type {
+	if typ, ok := t.typeMap[pkgname + "." + typename]; ok {
+		return typ
+	}
+	return nil
+}
+
+func (t *typeFactory) Del(typename, pkgname string) {
+	delete(t.typeMap, pkgname + "." + typename)
+}
+
+func (t *typeFactory) Clear() {
+	t.typeMap = make(map[string]reflect.Type)
+}
---
+++ typefactory_test.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2013 Ryan Chew. All rights reserved.
+// Use of this source code is governed by a Apache 2.0
+// license that can be found in the LICENSE file.
+
+package cbl
+
+import (
+	"testing"
+	"reflect"
+)
+
+type TestType struct {
+	TestValue int32
+	NameValue string
+	InnerValue InnerType
+}
+
+type InnerType struct {
+	Value float32
+}
+
+func TestTypeFactory(t *testing.T) {
+	defer TypeFactory.Clear()
+	TypeFactory.
+		Add(reflect.TypeOf(new(TestType))).
+		Add(reflect.TypeOf(new(InnerType)))
+
+	if newtype := TypeFactory.New("TestType", "cbl"); newtype != nil {
+		if _, ok := newtype.(TestType); !ok {
+			t.Errorf("Unable to create type: TestType")
+		}
+	}
+
+	if newtype := TypeFactory.New("InnerType", "cbl"); newtype != nil {
+		if _, ok := newtype.(InnerType); !ok {
+			t.Errorf("Unable to create type: InnerType")
+		}
+	}
+
+	if gettype := TypeFactory.Get("TestType", "cbl"); gettype == nil {
+		t.Errorf("Unable to get type: TestType")
+	}
+
+	if gettype := TypeFactory.Get("InnerType", "cbl"); gettype == nil {
+		t.Errorf("Unable to get type: InnerType")
+	}
+}
